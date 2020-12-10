0032744cc571975eb645a009c4cea804c24b3a64
Anuj Goyal <anujg935@gmail.com>
2020-12-10 23:55:05 +0530

    Some problems of leetcode december challange

---
+++ LeetCode/941. Valid Mountain Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        if(A.size()<3) return false;
+        
+        int pointer=1;
+        while(pointer<A.size()){
+            if(A[pointer-1]<A[pointer]) pointer++;
+            else break;
+        }
+
+        if(pointer==A.size()||pointer==1) return false;
+        while(pointer<A.size()){
+            if(A[pointer-1]>A[pointer]){
+                pointer++;
+            } else break;
+        }
+        
+        if(pointer==A.size()) return true;
+        else return false;
+    }
+};
